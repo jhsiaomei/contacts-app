dd7663766659e88f2b24f636d4fa189a467f0f2e
jhsiaomei <jw1314@gmail.com>
2015-11-15 13:46:22 -0600

    added bootstrap, built basic views of form, confirmation, display all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,24 @@
 class ContactsController < ApplicationController
+  def home
+    @title = "Home"
+  end
+
+  def display_all_contacts
+    @title = "Address Book"
+    @contact = Contact.all
+  end
+
+  def add_contact_form
+    @title = "Add New Contact"
+  end
+
+  def add_contact_confirmation
+    @title = "Add Contact Confirmation"
+    @contact = Contact.create(
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        phone: params[:phone]
+      )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/home' => 'contacts#home'
+  get '/display_all_contacts' => 'contacts#display_all_contacts'
+  get '/add_contact_form' => 'contacts#add_contact_form'
+  post '/add_contact_confirmation' => 'contacts#add_contact_confirmation'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
