c8c2657451b56415208a976673d12d2b8dcb6938
jhsiaomei <jw1314@gmail.com>
2015-11-16 20:26:04 -0600

    all routes and views connected via controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,17 +16,33 @@ class ContactsController < ApplicationController
         email: params[:email],
         phone: params[:phone]
       )
+
+    redirect_to '/contacts'
   end
 
   def show
+    @contact = Contact.find_by(id: params[:id])
+
   end
 
   def edit
+    @contact = Contact.find_by(id: params[:id])
   end
 
   def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        phone: params[:phone]
+      )
+
+    redirect_to '/contacts'
   end
 
   def destroy
+    contact = Contact.find_by(id: params[:id])
+    contact.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/contacts' => 'contacts#index'
   get '/contacts/new' => 'contacts#new'
-  post '/contacts/' => 'contacts#create'
+  post '/contacts' => 'contacts#create'
   get '/contacts/:id' => 'contacts#show'
   get '/contacts/:id/edit' => 'contacts#edit'
   patch '/contacts/:id' => 'contacts#update'
