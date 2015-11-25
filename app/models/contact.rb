f22388b1bf1a2ca4d32c5cd1978e252c8256c57a
jhsiaomei <jw1314@gmail.com>
2015-11-24 19:12:02 -0600

    migrated from SQLite3 to MySQL, added sign up/in/out to navbar

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+  belongs_to :user
+
   def display_created_time
     created_at.strftime("%m/%e/%Y at %l:%M %p")
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# User.create!([
+#   {email: "test@email.com", encrypted_password: "$2a$10$Shr6DC/vpdjQ8Ph4d0GfWeB51C63TueiDXxVP6LadV2s3eWs1T1rq", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-11-25 00:31:42", last_sign_in_at: "2015-11-25 00:31:42", current_sign_in_ip: "::1", last_sign_in_ip: "::1"}
+# ])
+Contact.create!([
+  {first_name: "Prince", last_name: "Aladdin", email: "carpet@newworld.com", phone: "412-867-5309"},
+  {first_name: "Red", last_name: "Badgro", email: "red.badgro@email.com", phone: "123-456-7890"},
+  {first_name: "Ali", last_name: "Baba", email: "ali@agarwal.com", phone: "234-435-5732"},
+  {first_name: "Donald", last_name: "Duck", email: "quack@feathers.com", phone: "213-325-4569"},
+  {first_name: "Minnie", last_name: "Mouse", email: "minnie@mouseears.com", phone: "234-453-1230"},
+  {first_name: "Bambi's", last_name: "Mother", email: "doe@forest.com", phone: "354-763-4356"}
+])
